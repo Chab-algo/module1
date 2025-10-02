4c8939227885030937a1193afab761cc0b74d8a7
BenjaminC <bchabanis@eugeniaschool.com>
2025-10-02 13:14:50 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+# age = 21
+# height = 1.89
+# name = "benjamin"
+# is_student = True
+# print(height)
+# print(name)
+# print(age)
+# print(is_student)
+
+# a = 2
+# b = 3
+# ddition = a + b
+# subtraction = a - b
+# multiplication = a * b
+# division = a / b
+
+# print(f"a+b={a+b}")
+# print(f"a-b={a-b}")
+# print(f"a*b={a*b}")
+# print(f"a/b={a/b}")
+# print(f"rest de a /b={a % b}")
+
+# first_name = "benjamin"
+# last_name = "chabanis"
+# full_name = first_name+" "+last_name
+
+# print("Full Name (Uppercase):", full_name.upper())
+
+# sentence = "Benjamin Chabanis dev"
+# words_list = sentence.split()
+# print("Split Sentence:", words_list)
+
+
+# caractère = len(full_name)
+# len(full_name)
+# print(f"caractère={caractère}")
+
+
+# string = "g"
+# integer = int(string)
+# floats = 9.3
+# integer2 = int(floats)
+# print(integer, integer2)
+
+# str_number = "42"
+# int_from_str = int(str_number)
+
+# float_number = 3.99
+# int_from_float = int(float_number)
+
+# int_number = 10
+# float_from_int = float(int_number)
+
+# num = 7.5
+# str_from_num = str(num)
+
+# print(f"Integer from string: {int_from_str}, Type: {type(int_from_str)}")
+# print(f"Integer from float: {int_from_float}, Type: {type(int_from_float)}")
+# print(f"Float from integer: {float_from_int}, Type: {type(float_from_int)}")
+# print(f"String from number: '{str_from_num}', Type: {type(str_from_num)}")
+
+
+# question 5
+
